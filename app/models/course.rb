e2dcd1fe9efe0a8b6e695d0608e0adce18bd9923
Emanoel Lopes <emanoelopes@gmail.com>
2019-06-11 16:45:50 -0300

    Created partials from index.html

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
-
+	validates :name, presence: true, length: { minimun: 5, maximum: 10 }
+	validates :short_name, presence: true, length: { minimun: 3, maximum: 50 }
+	validates :description_name, presence: true, length: { minimun: 10, maximum: 300 }
 end
