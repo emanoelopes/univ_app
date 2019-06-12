13a3cb1541d9778b8f70072ac9406fefe9bd7cbf
Emanoel Lopes <emanoelopes@gmail.com>
2019-06-12 16:31:22 -0300

    Students can be created from forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
   # For details on thdde DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, except: [:destroy]
 end
