e73152ccc72f48e6d4b9676588e58b3eee1d2472
Emanoel Lopes <emanoelopes@gmail.com>
2019-06-17 15:05:13 -0300

    Authentication system - add auth methods

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # For details on thdde DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, except: [:destroy]
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
 end
