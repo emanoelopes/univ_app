d9b321a152f4763c4e632a11f89970b7f1e9dac9
Emanoel Lopes <emanoelopes@gmail.com>
2019-06-18 16:28:14 -0300

    Last lesson and finished the unit UNIV APP

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,7 @@ class Course < ApplicationRecord
 	validates :name, presence: true, length: { minimun: 5, maximum: 10 }
 	validates :short_name, presence: true, length: { minimun: 3, maximum: 50 }
 	validates :description_name, presence: true, length: { minimun: 10, maximum: 300 }
+
+	has_many :student_courses
+	has_many :students, through: :student_courses
 end
